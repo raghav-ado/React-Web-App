85cee95dfed135f3ed95e0ff0a6e5aba30f40be4
Raghav Sharma <raghavsharmadnn@gmail.com>
2020-06-29 21:05:50 +0530

    Updates

---
+++ precache-manifest.a6b84b887822a379762a26c5732b005a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc1b8e4f059b0018a785ce115456c74",
+    "url": "/React-Web-App/index.html"
+  },
+  {
+    "revision": "cc37b7abf37829b5aaf3",
+    "url": "/React-Web-App/static/css/main.b5a45aba.chunk.css"
+  },
+  {
+    "revision": "c343a0429b5c10082f1d",
+    "url": "/React-Web-App/static/js/2.ce84c031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Web-App/static/js/2.ce84c031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc37b7abf37829b5aaf3",
+    "url": "/React-Web-App/static/js/main.cb7a0c12.chunk.js"
+  },
+  {
+    "revision": "1ad0ac5a804b41b0c6f2",
+    "url": "/React-Web-App/static/js/runtime-main.08eb6434.js"
+  }
+]);
